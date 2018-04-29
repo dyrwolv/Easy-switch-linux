6d102ebf09415a20b7c01f8d9b9b37e882750a8e
dyrwolv <38821259+dyrwolv@users.noreply.github.com>
2018-04-29 02:25:22 -0500

    Add files via upload

--- boot.sh
+++ boot.sh
@@ -4,6 +4,7 @@ cd ./switch-linux
 cd shofel2/exploit
 sudo ./shofel2.py cbfs.bin ../../coreboot/build/coreboot.rom
 cd ../ 
-cd shofel2/usb_loader
+cd ./usb_loader
+echo $PWD
 ../../u-boot/tools/mkimage -A arm64 -T script -C none -n "boot.scr" -d switch.scr switch.scr.img
 sudo ../../imx_usb_loader/imx_usb -c .
